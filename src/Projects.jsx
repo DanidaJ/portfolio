9d5cacc044fee4cc80c2534fb8e850149649d0ea
Danida <jayakodydanida@gmail.com>
2025-02-14 22:41:30 +0530

    commit

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -13,7 +13,7 @@ export default function Projects() {
             id: 1,
             title: "Cinema Ticket Booking System",
             description: "A web app to book cinema tickets online with seat selection.",
-            image: "/src/assets/ticketbooking.png", // Place image in public/images/
+            image: "https://github.com/DanidaJ/portfolio/blob/c8cd628d5f5e34378677e1dfd6fb7b8ad454b409/src/assets/ticketbooking.png", // Place image in public/images/
             // link: "https://yourcinemaapp.com",
         },
         {
